8cc9697c7325b6bf35591518c440812ff291201c
cnguyen934 <55369747+cnguyen934@users.noreply.github.com>
2020-05-08 13:06:08 -0700

    Update the soup with main and functions

--- canadaSoup.py
+++ canadaSoup.py
@@ -1,34 +1,42 @@
 import requests
 from bs4 import BeautifulSoup
-import lxml.html as lh
 import csv
-
-# Scraping table data
+import re
 
 url_data = 'https://www.canada.ca/en/public-health/services/diseases/2019-novel-coronavirus-infection.html?topic=tilelink'
-
 page = requests.get(url_data)
+canadaData = open("canadaData.csv", "w")
+canadaData.close()
+canada_list = []
 
 soup = BeautifulSoup(page.content, 'html.parser')
-table = soup.find('table', class_='table table-striped table-bordered table-hover')
+table = soup.find('table', class_ = 'table table-striped table-bordered table-hover')
+prov_elems = table.find_all('tr')
 
-canada_list = []
-for row in table.find_all('tr'):
-    province_list = []
-    for cell in row.find_all('td'):
-        province_list.append(cell.text)
-    canada_list.append(province_list)   
+def scrapeCanada():
+    canada_list.append(["Province Name", "Number of Cases", "Number of Deaths"])
+    for prov_elem in prov_elems:
+        td_list = prov_elem.find_all('td')
+        if(td_list):
+            prov_name = re.sub('<|t|d|>|/', '', str(td_list[0]))
+            prov_cases = re.sub('<|t|d|>|/', '', str(td_list[1]))
+            prov_deaths = re.sub('<|t|d|>|/', '', str(td_list[3]))
+            canada_list.append([prov_name, prov_cases, prov_deaths])
+    
+    return canada_list  
+    canadaToCSV()
+    
+def canadaToCSV():
 
-print(canada_list)
+    with open("canadaData.csv", 'w', newline = '') as fout:
+        covid_writer = csv.writer(fout)
+        for each in canada_list:
+            covid_writer.writerow([each[0], each[1], each[2]])
+    canadaData.close()
 
-data_file = "canadaData.csv"
+if __name__ == '__main__':
 
-try:
-
-    with open(data_file, 'w') as wFile:
-        writer = csv.writer(wFile)
-        writer.writerow(["Province", "Confirmed Cases", "Probable Cases", "Deaths"])
-        writer.writerows(canada_list)
+    scrapeCanada()
                    
-except OSError:
-    print("Cannot open", data_file)
+
+
